19a64ace024723ec34e3777e7c2dc77185d308f9
Sergey <93572822+JusteRYT@users.noreply.github.com>
2024-11-04 15:09:53 +0300

    Добавление метода findAllByDateBetweenAndStatus в TodoRepository

--- src/main/java/com/example/TodoListApp/repository/TodoRepository.java
+++ src/main/java/com/example/TodoListApp/repository/TodoRepository.java
@@ -4,10 +4,14 @@ import com.example.TodoListApp.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * Репозиторий для операций CRUD с таблицей задач.
  */
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-
+    List<TodoEntity> findAllByDateBetweenAndStatus(LocalDateTime from, LocalDateTime to, Boolean status);
 }
