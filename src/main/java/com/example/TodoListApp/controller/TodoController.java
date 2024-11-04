3208b9f59bca79e28cd3568e2c5b72991f887d5b
Sergey <93572822+JusteRYT@users.noreply.github.com>
2024-11-04 15:14:59 +0300

    Добавление эндпоинта для фильтрации задач по дате и статусу в TodoController

--- src/main/java/com/example/TodoListApp/controller/TodoController.java
+++ src/main/java/com/example/TodoListApp/controller/TodoController.java
@@ -3,10 +3,13 @@ package com.example.TodoListApp.controller;
 import com.example.TodoListApp.entity.TodoEntity;
 import com.example.TodoListApp.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -18,16 +21,25 @@ public class TodoController {
     private final TodoService todoService;
 
     @Autowired
-    public TodoController(TodoService todoService){
+    public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
 
     /**
      * Получение списка всех задач.
+     *
      * @return список задач.
      */
     @GetMapping
-    public List<TodoEntity> getAllTodos(){
+    public List<TodoEntity> getAllTodos() {
         return todoService.getAllTodos();
+    }
+
+    @GetMapping("/date")
+    public List<TodoEntity> getTodosByDateAndStatus(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to,
+            @RequestParam Boolean status) {
+        return todoService.getTodosByDateAndStatus(from, to, status);
     }
 }
